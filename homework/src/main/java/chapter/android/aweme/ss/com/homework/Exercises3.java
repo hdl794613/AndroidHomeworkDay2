00802b94a56da09b774bb9a5db5f4d2691819bf9
hdl794613 <243662291@qq.com>
2019-07-12 23:20:33 +0800

    Initial commit

---
+++ homework/src/main/java/chapter/android/aweme/ss/com/homework/Exercises3.java
@@ -0,0 +1,34 @@
+package chapter.android.aweme.ss.com.homework;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+/**
+ * 大作业:实现一个抖音消息页面,所需资源已放在res下面
+ */
+public class Exercises3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tips);
+
+        ListView listView = findViewById(R.id.list_view);
+        listView.setAdapter(new ListAdapter(this));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(Exercises3.this, "当前位置" + position, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Exercises3.this, ChartRoom.class);
+                intent.putExtra("position", position);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
